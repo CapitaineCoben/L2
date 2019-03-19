37372321c36566316151a1d18fec3fa9258adbf6
CapitaineCoben <homosapionse@gmail.com>
2019-03-19 08:57:25 +0100

    test 19/03/2019

--- S4/POO/TP/TP3/vehicles.cpp
+++ S4/POO/TP/TP3/vehicles.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 
 class vehicles_t{
+  static float nkm;
   public :float fuel,fuel_cons,fuel_max;
   public :char* name;
   public : vehicles_t(char* name,float f,float f_c,float f_m){
@@ -9,9 +10,10 @@ class vehicles_t{
       this->fuel_max = f_m;
       this->name = name;
   }
-  bool move(float km){
+  bool move(float km = 1){
     if (km>0 and (km*this->fuel_cons/100)<this->fuel) {
       this->fuel -= (km*this->fuel_cons/100);
+      nkm += km;
       printf("car moved (%.2f km).\n",km);
       return true;
     }else{
@@ -26,6 +28,9 @@ class vehicles_t{
      this->fuel += l;
      return true;
     }
+  }
+  public : static float get_nkm(){
+    return nkm;
   }
   virtual void print() = 0;
 
@@ -44,19 +49,25 @@ class vehicles_t{
 };
 
 class car_t : public vehicles_t{
+  static float nkm;
   public :bool hand_brake_enable;
 
+  public :
+  static float get_nkm(){
+    return nkm;
+  }
   public :car_t(char* name,float fuel,float fuel_cons,float fuel_max,bool hand_brake_enable): vehicles_t(name,fuel,fuel_cons,fuel_max){
     this->hand_brake_enable = hand_brake_enable;
   }
   public :void set_hand_brake_enable(bool hand_brake_enable){
     this->hand_brake_enable = hand_brake_enable;
   }
-  public :bool move(float km){
+  public :bool move(float km = 1){
     if (this->hand_brake_enable) {
       printf("unable to move the car (%.2f)\n", km);
       return false;
     }else{
+      nkm += km;
       return(vehicles_t::move(km));
     }
   }
@@ -69,12 +80,12 @@ int main(int argc, char const *argv[]) {
   car_t mca((char*)"Mercedes A-Class",1.7,11,2.10,0);
   car_t bmw((char*)"BMW X4",57.2,11.30,65,0);
   mca.print();
-  mca.move(1);
+  mca.move();
   mca.print();
   mca.move(100);
   mca.print();
   bmw.print();
-  bmw.move(1);
+  bmw.move();
   bmw.print();
   bmw.move(100);
   bmw.print();
@@ -86,5 +97,8 @@ int main(int argc, char const *argv[]) {
   bmw.refuel(19.21);
   mca.print();
   bmw.print();
+  vehicles_t::get_nkm();
+  printf("vehicles_t::get_nkm(): %f\n", vehicles_t::get_nkm());
+  printf("car_t::get_nkm(): %f\n", car_t::get_nkm());
   return 0;
 }
